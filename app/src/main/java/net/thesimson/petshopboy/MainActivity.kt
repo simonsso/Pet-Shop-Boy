b4fcf95f918e763eafea312bbfcf16aa9d6c457b
Fredrik Simonsson <simson@thesimson.net>
2020-02-08 21:47:17 +0900

    Add recyclerview and assets for configuration files

--- app/src/main/java/net/thesimson/petshopboy/MainActivity.kt
+++ app/src/main/java/net/thesimson/petshopboy/MainActivity.kt
@@ -2,11 +2,18 @@ package net.thesimson.petshopboy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // One time disposable buttons ;-)
+        chat_button.setOnClickListener { chat_button.visibility = View.GONE }
+        call_button.setOnClickListener { call_button.visibility = View.GONE }
+
     }
 }
