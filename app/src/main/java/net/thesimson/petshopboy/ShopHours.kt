4f17508495f3fb78df0be9aaa912ec21b3a41917
Fredrik Simonsson <simson@thesimson.net>
2020-02-09 19:46:59 +0900

    Whops, Retrieving groups by name is not supported on this platform.

--- app/src/main/java/net/thesimson/petshopboy/ShopHours.kt
+++ app/src/main/java/net/thesimson/petshopboy/ShopHours.kt
@@ -8,16 +8,23 @@ object ShopHours {
     var closeAt:Int = 0
 
     fun parseBuisinessHours(s:String):Unit{
-
-        val pattern = Regex("""(?<days>[\w-]+)\s+(?<openh>\d+)(:(?<openm>\d+))?\s*-\s*(?<closeh>\d+)(:(?<closem>\d+))?""")
+        // Whops, Retrieving groups by name is not supported on this platform.
+        // Use group numbers
+        // Days: 1
+        // OpenHour: 2
+        // OpenMinute: 4
+        // CloseHour: 5
+        // CloseMinute:7
+        //  https://regex101.com/r/tBRHnN/1
+        val pattern = Regex("""([\w-]+)\s+(\d+)(:(\d+))?\s*-\s*(\d+)(:(\d+))?""")
 
         val mat:MatchResult? =pattern.matchEntire(s)
         if (mat != null ){
             val groups = mat.groups
-            val openHour:Int = groups["openh"]?.value?.toIntOrNull()?:0
-            val openMin:Int = groups["openm"]?.value?.toIntOrNull()?:0
-            val closeHour:Int = groups["closeh"]?.value?.toIntOrNull()?:0
-            val closeMin:Int = groups["closem"]?.value?.toIntOrNull()?:0
+            val openHour:Int = groups[2]?.value?.toIntOrNull()?:0
+            val openMin:Int = groups[4]?.value?.toIntOrNull()?:0
+            val closeHour:Int = groups[5]?.value?.toIntOrNull()?:0
+            val closeMin:Int = groups[7]?.value?.toIntOrNull()?:0
 
             openAt = 60 * openHour + openMin
             closeAt = 60 * closeHour + closeMin
